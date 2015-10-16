0fef49975e8d02de0e7bbdc40301ce5f57b37050
Johan Olby <johan@olby.se>
2015-10-16 18:03:46 +0200

    Initial commit

---
+++ src/main/java/se/olby/innometrics/NamedCounter.java
@@ -0,0 +1,17 @@
+package se.olby.innometrics;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+ 
+@Path("/entry-point")
+public class NamedCounter {
+ 
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Test";
+    }
+}
---
+++ src/main/java/se/olby/innometrics/Server.java
@@ -0,0 +1,27 @@
+package se.olby.innometrics;
+
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+ 
+        org.eclipse.jetty.server.Server jettyServer = new org.eclipse.jetty.server.Server(8080);
+        jettyServer.setHandler(context);
+ 
+        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+ 
+        jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", NamedCounter.class.getCanonicalName());
+ 
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+    }
+}
