47625968862550f8a049db9cc5ba725a84cc4f36
Johan Olby <johan@olby.se>
2015-10-17 14:11:36 +0200

    Sketch for API entry points

--- src/main/java/se/olby/innometrics/NamedCounter.java
+++ src/main/java/se/olby/innometrics/NamedCounter.java
@@ -1,17 +1,47 @@
 package se.olby.innometrics;
  
 import javax.ws.rs.GET;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
  
-@Path("/entry-point")
+@Path("/")
 public class NamedCounter {
- 
+
+    /** List counters **/ 
     @GET
-    @Path("test")
+    @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
-    public String test() {
-        return "Test";
+    public String list() {
+        return "";
     }
+
+    /** Init a counter **/ 
+    @GET
+    @Path("{counter}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String init(@PathParam("counter") String counter) {
+        return counter;
+    }
+
+    /** Delete a counter **/ 
+    @DELETE
+    @Path("{counter}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String delete(@PathParam("counter") String counter) {
+        return counter;
+    }
+
+    /** Increment a counter and returns the value prior to the increment, CAS sematics can be implementet with an extra parameter (old valus)**/ 
+    @POST
+    @Path("{counter}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String increment(@PathParam("counter") String counter) {
+        return counter;
+    }
+
 }
